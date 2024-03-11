4dcdf602d4d96668d5351c63a895a6243533d042
reinhardt-bit <reinhartc@tutanota.com>
2024-03-11 22:53:29 +0200

    working
    templ up and running

--- app/main.go
+++ app/main.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	passMan "github.com/reinhardt-bit/PassMan"
+	"github.com/reinhardt-bit/PassMan/app/views/dashboard"
 	torsoSql "github.com/reinhardt-bit/PassMan/internal/sql"
-	"github.com/reinhardt-bit/passMan/app/views/dashboard"
 )
 
 func main() {
 	app := passMan.New()
 
-	app.Get("/2fa", Handle2FAIndex)
+	app.Get("/dashboard", Handle2FAIndex)
 
 	app.Start(":3000")
 	// torsoUrl := torsoSql.GetTorsoURL("my-db")
@@ -27,4 +27,5 @@ func main() {
 func Handle2FAIndex(c *passMan.Context) error {
 	// ctx.HTML(templates.Index)
 	return c.Render(dashboard.Index())
+	// return c.Render(dashboard.index())
 }
--- passMan.go
+++ passMan.go
@@ -9,26 +9,26 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-type Context struct {
-	response http.ResponseWriter
-	request  *http.Request
-	ctx      context.Context
-}
-
 func defaultErrorHandler(err error, c *Context) error {
 	slog.Error("error", "err", err)
 	return nil
 }
 
 type (
-	ErrorHandler func(err error, c *Context)
+	ErrorHandler func(error, *Context) error
 	thisones     interface {
 		PassMan
 	}
 )
 
-func (c *Context) Renderer(component templ.Component) error {
-	return component.Render(c.response, c.request)
+type Context struct {
+	response http.ResponseWriter
+	request  *http.Request
+	ctx      context.Context
+}
+
+func (c *Context) Render(component templ.Component) error {
+	return component.Render(c.ctx, c.response)
 }
 
 type Handler func(c *Context) error
@@ -36,6 +36,21 @@ type Handler func(c *Context) error
 type PassMan struct {
 	ErrorHandler ErrorHandler
 	router       *httprouter.Router
+}
+
+func New() *PassMan {
+	return &PassMan{
+		router:       httprouter.New(),
+		ErrorHandler: defaultErrorHandler,
+	}
+}
+
+func (s *PassMan) Start(port string) error {
+	return http.ListenAndServe(port, s.router)
+}
+
+func (s *PassMan) Get(path string, h Handler, plugs ...Handler) {
+	s.router.GET(path, s.makeHTTPRouterHandle(h))
 }
 
 func (s *PassMan) makeHTTPRouterHandle(h Handler) httprouter.Handle {
